d358e167b751244fad971d89ea3664a6f0839dd0
Erin Winningham <34450980+policyoftruth@users.noreply.github.com>
2023-01-16 01:38:01 +0000

    set up some needed binary downloads

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-getLatestVersion() {
-LATEST_ARR=($(wget -q -O- https://api.github.com/repos/hashicorp/terraform/releases 2> /dev/null | awk '/tag_name/ {print $2}' | cut -d '"' -f 2 | cut -d 'v' -f 2))
-for ver in "${LATEST_ARR[@]}"; do
-  if [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]]; then
-    LATEST="$ver"
-    break
-  fi
-done
-echo -n "$LATEST"
-}
+### versions
+terraform_version="1.3.7"
+terragrunt_version="0.42.8"
 
-VERSION=$(getLatestVersion)
+echo "terraform version is ${terraform_version}"
+echo "terragrunt version is ${terragrunt_version}"
 
-cd ~
-wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
-unzip "terraform_"$VERSION"_linux_amd64.zip"
+### fetch terraform
+wget -q "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip" -O ./terraform.zip
+unzip -o ./terraform.zip > /dev/null 2>&1
 sudo install terraform /usr/local/bin/
+rm -f ./terraform.zip ./terraform
+
+### fetch terragrunt
+wget -q "https://github.com/gruntwork-io/terragrunt/releases/download/v${terragrunt_version}/terragrunt_linux_amd64" -O ./terragrunt
+sudo install terragrunt /usr/local/bin/
+rm -f ./terragrunt
+
