77d498f9880ec60a82fe08c600a201fca7f42c19
Erin Winningham <34450980+policyoftruth@users.noreply.github.com>
2023-01-22 01:48:00 +0000

    Added Azure tf stuffs

---
+++ bootstrap/SETUP.md
@@ -0,0 +1,8 @@
+### commands to create an sp
+
+* `az login --use-device-code`
+* `az account list-o table`
+* `az account set --subscription="SUBSCRIPTION_ID"`
+* `az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/SUBSCRIPTION_ID"`
+
+### need a storage account for the backend as well
--- provider.tf
+++ provider.tf
@@ -1,9 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.40.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-projects"
+    storage_account_name = "ejwterraformstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
