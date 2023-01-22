538e76508ab379379eab348e0ba26716bcbc2858
Erin Winningham <34450980+policyoftruth@users.noreply.github.com>
2023-01-22 00:44:55 +0000

    add azcli and whatnot

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-### versions
-terraform_version="1.3.7"
-terragrunt_version="0.42.8"
-
-echo "terraform version is ${terraform_version}"
-echo "terragrunt version is ${terragrunt_version}"
-
-### fetch terraform
-wget -q "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip" -O ./terraform.zip
-unzip -o ./terraform.zip > /dev/null 2>&1
-sudo install terraform /usr/local/bin/
-rm -f ./terraform.zip ./terraform
-
-### fetch terragrunt
-wget -q "https://github.com/gruntwork-io/terragrunt/releases/download/v${terragrunt_version}/terragrunt_linux_amd64" -O ./terragrunt
-sudo install terragrunt /usr/local/bin/
-rm -f ./terragrunt
-
+echo "Add stuff here if needed!"
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # terragrunt-azure
 
 This is a repository to test terragrunt/terraform patterns on the Azure cloud
+
+* ghcr.io/devcontainers/features/azure-cli:1	
+* https://containers.dev/features
+* ghcr.io/devcontainers/features/terraform:1	
