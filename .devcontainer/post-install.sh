c7285b4fbc31f7a6662c2c8d55968e19412da57a
Erin Winningham <34450980+policyoftruth@users.noreply.github.com>
2023-02-10 01:37:15 +0000

    remove un-needed package

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-command -v gvcolor >/dev/null 2>&1 || { sudo apt install graphviz -y; exit 1; }
+# command -v gvcolor >/dev/null 2>&1 || { sudo apt install graphviz -y; exit 1; }
+echo "Hello World!"
